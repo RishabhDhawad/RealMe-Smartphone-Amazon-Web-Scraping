46462bc21ee089a2c59c2f0b3c4e0bf82b5dddf2
RishabhDhawad <rishabhdhawad29@gmail.com>
2025-05-18 23:01:47 +0530

    updated: filter and multiple page scraping

--- webscraping.py
+++ webscraping.py
@@ -7,14 +7,12 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-        "(KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
-    ),
-    "Accept-Language": "en-US,en;q=0.9"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)...",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.google.com"
 }
 
-BASE_URL = "https://www.amazon.in/s?k=realme+mobile&page={}"
+BASE_URL = "https://www.flipkart.com/mobiles/realme~brand/pr?sid=tyy%2C4io&page={}"
 
 titles = []
 prices = []
@@ -23,30 +21,29 @@ for page in range(1, 6):
     print(f"Scraping page {page}...")
     url = BASE_URL.format(page)
 
-    response = requests.get(url, headers=HEADERS,  timeout=10)
+    response = requests.get(url, headers=HEADERS)
 
     if response.status_code != 200:
         print(f"Failed to fetch page {page}")
         continue
 
     soup = BeautifulSoup(response.content, "html.parser")
-    products = soup.find_all("div", {"data-component-type": "s-search-result"})
+    products = soup.find_all("div", class_="tUxRFH")
+    print(f"Found {len(products)} products on page {page}")
 
     for product in products:
-        title_tag = product.h2
+        title_tag = product.find("div", class_="KzDlHZ")
         title = title_tag.text.strip() if title_tag else "No Title"
 
-
-        price_tag = product.select_one("span.a-price > span.a-offscreen")
+        price_tag = product.find("div", class_="Nx9bqj _4b5DiR")
         price = price_tag.text.strip() if price_tag else "No Price"
-
 
         titles.append(title)
         prices.append(price)
 
     time.sleep(1)
 
-#clean the price string to integers
+#clean the price string
 def clean_price(price_str):
     try:
         price_num = price_str.replace("₹", "").replace(",","").strip()
@@ -66,6 +63,9 @@ df = pd.DataFrame({
 df = df.dropna(subset=["Price"])
 filtered_df = df[df["Price"] > 15000]
 
+print(f"Titles collected: {len(titles)}")
+print(f"Prices collected: {len(prices)}")
 
+# import pdb; pdb.set_trace()
 filtered_df.to_excel("realme_smartphones.xlsx", index=False)
 print("Scraping complete. Data saved to realme_smartphones.xlsx")
